4e98df83fea51d414a433b96ecb476b5f6517879
fabiodplacerda <lacerda96@gmail.com>
2024-04-19 15:48:08 +0100

    NEW: implemented isAirportFull functionality along with corresponding tests.

--- spec/airport.test.js
+++ spec/airport.test.js
@@ -4,6 +4,7 @@ import airport from "../src/airport.js";
 // After Each test function
 const afterEach = () => {
   airport.airportCapacity = 10;
+  airport.planesAtAirport = [];
 };
 
 //? Test 1
@@ -31,6 +32,8 @@ console.log(`==================`);
 // Clean Up
 afterEach();
 
+//! END OF TEST 1
+
 //? Test 2
 console.log(
   "TEST 2: Check if changeCapacity() doesn't modify the capacity if it's called with no number"
@@ -56,6 +59,8 @@ console.log(`==================`);
 // Clean Up
 afterEach();
 
+//! END OF TEST 2
+
 //? Test 3
 console.log(
   "TEST 3: Check if changeCapacity() doesn't modify the capacity if the number passed in is lower than 0"
@@ -80,3 +85,60 @@ console.log(`==================`);
 
 // Clean Up
 afterEach();
+
+//! END OF TEST 3
+
+//? Test 4
+console.log(
+  "TEST 4: test that checks if the airport is full using isAirportFull() and if it is it should return true"
+);
+console.log(`==================`);
+
+// Arrange
+airport.airportCapacity = 2;
+let planesArrayTest = [{ planeId: "1" }, { planeId: "2" }];
+expected = true;
+
+// Act
+airport.planesAtAirport = planesArrayTest;
+actual = airport.isAirportFull();
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Result
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+//! END OF TEST 4
+
+//? Test 5
+console.log(
+  "TEST 5: test that checks if the airport is full using isAirportFull() and if it isn't it should return false"
+);
+console.log(`==================`);
+
+// Arrange
+planesArrayTest = [{ planeId: "1" }, { planeId: "2" }];
+expected = false;
+
+// Act
+airport.planesAtAirport = planesArrayTest;
+actual = airport.isAirportFull();
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Result
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+//! END OF TEST 5
--- src/airport.js
+++ src/airport.js
@@ -1,9 +1,13 @@
 const airport = {
   airportCapacity: 10,
+  planesAtAirport: [],
   changeCapacity: function (newCapacity) {
     if (typeof newCapacity === "number" && newCapacity >= 0) {
       this.airportCapacity = newCapacity;
     }
+  },
+  isAirportFull: function () {
+    return this.planesAtAirport.length >= this.airportCapacity;
   },
 };
 
