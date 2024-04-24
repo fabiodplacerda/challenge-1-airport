76370df56923ace625a89f5e9c5cbb16f73eba50
fabiodplacerda <lacerda96@gmail.com>
2024-04-24 16:13:03 +0100

    NEW: new testing function to compare two objects

--- spec/test-framework/test-framework.js
+++ spec/test-framework/test-framework.js
@@ -1,2 +1,4 @@
 export const assertEquals = (actual, expected) => actual === expected;
 export const assertNotEquals = (actual, expected) => actual !== expected;
+export const toEqual = (actual, expected) =>
+  actual.toString() === expected.toString();
